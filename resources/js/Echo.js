1bbb6f83afabdfe008d930042a75fee1a5508d98
lbsys <lucas.barbeiro@gmail.com>
2022-12-12 19:49:12 -0300

    Notificação com VueToastify

--- resources/js/Echo.js
+++ resources/js/Echo.js
@@ -1,4 +1,9 @@
+import Vue from 'vue';
+
 window.Echo.channel('news_database_post-created')
     .listen('PostCreated', (e) => {
         console.log(e)
-    });
+        console.log(e.post)
+
+        Vue.$vToastify.success(`Titulo do Post ${e.post}`, 'Novo Post').default;
+    })
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+
+import Vue from 'vue';
+import VueToastify from 'vue-toastify';
+
+export default {
+    name: 'App',
+};
+
+Vue.use(VueToastify)
+
+/*
+    const app = Vue({});
+    app.use(VueToastify);
+    app.mount('#App')
+*/
